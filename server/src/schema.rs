7886916885f82adb06a16fd3d0a88663d140f34a
GimpFlamingo <rayjkochenderfer@gmail.com>
2020-08-31 10:08:07 -0400

    Update schema.rs

--- server/src/schema.rs
+++ server/src/schema.rs
@@ -1,4 +1,12 @@
 table! {
+    sessions (id) {
+        id -> Int4,
+        user_id -> Int4,
+        token -> Varchar,
+    }
+}
+
+table! {
     users (id) {
         id -> Int4,
         email -> Varchar,
@@ -10,3 +18,10 @@ table! {
         updated_at -> Timestamp,
     }
 }
+
+joinable!(sessions -> users (user_id));
+
+allow_tables_to_appear_in_same_query!(
+    sessions,
+    users,
+);
