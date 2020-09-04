ee82b0b225c4eaaab526f9f5e064b7d7b2082dc3
GimpFlamingo <rayjkochenderfer@gmail.com>
2020-09-04 08:40:07 -0400

    Add react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Layout} from "./components/Layout";
 import './App.css';
 import Login from "./components/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" render={(props) => <Login {...props} />} />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
